c66fc82b7a5ae98c5a2e08376cc404587f9d80e5
Kris Anderson <1562427+kris-anderson@users.noreply.github.com>
2025-05-03 01:23:15 -0700

    fix: update content paths in Tailwind configuration for improved file matching

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/templates/**/*.html", "./src/themes/**/*.js", "./src/templates/template.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
